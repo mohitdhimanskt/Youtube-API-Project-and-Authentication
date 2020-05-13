49956cf6b662d94835cb8786e23f5f9b0d5c4e2a
Mohit <mohit.dhiman.sarkaghat@gmail.com>
2020-05-13 16:22:10 +0530

    Fourteencommit

--- index.js
+++ index.js
@@ -35,3 +35,17 @@ function initClient(){
         signoutButton.onclick = handleSignoutClick;
     })
 }
+function updatedSigninStatus(isSignedIn){
+    if(isSignedIn){
+        authorizeButton.style.display = 'none';
+        signoutButton.style.display = 'block';
+        content.style.display = 'block';
+        videoContainer.style.display = 'block';
+        getChannel(defaultChannel);
+    }else {
+        authorizeButton.style.display = 'block';
+        signoutButton.style.display = 'none';
+        content.style.display = 'none';
+        videoContainer.style.display = 'none';
+    }
+}
