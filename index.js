f98f0ef9e55c4dd664946443b8a723e10e2087dc
Mohit <mohit.dhiman.sarkaghat@gmail.com>
2020-05-13 21:47:40 +0530

    Seventeencommit

--- index.js
+++ index.js
@@ -60,3 +60,39 @@ function showChannelData(data){
     const channelData = document.getElementById('channel-data');
     channelData.innerHTML = data;
 }
+function getChannel(channel){
+    gapi.client.youtube.channels
+    .list({
+        part: 'snippet,contentDetails,statistics',
+        forUsername: channel
+    })
+    .then(response => {
+        console.log(response);
+        const channel = response.result.items[0];
+        const output = `
+        <ul class="collection">
+          <li class="collection-item">Title: ${channel.snippet.title}</li>
+          <li class="collection-item">ID: ${channel.id}</li>
+          <li class="collection-item">Subscribers: ${numberWithCommas(
+            channel.statistics.subscriberCount
+          )}</li>
+          <li class="collection-item">Views: ${numberWithCommas(
+            channel.statistics.viewCount
+          )}</li>
+          <li class="collection-item">Videos: ${numberWithCommas(
+            channel.statistics.videoCount
+          )}</li>
+        </ul>
+        <p>${channel.snippet.description}</p>
+        <hr>
+        <a class="btn grey darken-2" target="_blank" href="https://youtube.com/${
+          channel.snippet.customUrl
+        }">Visit Channel</a>
+      `;
+      showChannelData(output);
+
+      const playlistId = channel.contentDetails.relatedPlaylists.uploads;
+      requestVideoPlaylist(playlistId);
+    })
+    .catch(err => alert('No Channel By That Name'));
+}
